a130caf5af7cf0bdeba4c0dcf199f21dc5793601
root <root@next-hyd-9518.nexteducation.com>
2018-10-22 19:50:19 +0530

    code cleanup

--- server.js
+++ server.js
@@ -5,30 +5,23 @@ const path = require('path')
 
 
 dir=pathofIndexfile;
-filePath= pathifVideoFile;
+filePath= pathofVideofile;
 
 app.get("/samplePlayer",function(request,response){
 	response.sendFile(path.join(dir+ '/index.html'))
 })
 
-//streaming the file
 app.get("/video",function(request,response){
-
-	console.log(request.headers.range);
 	var stat = fs.statSync(filePath)
 	var range = request.headers.range;
 	var size= stat.size;
 	if(range){
 		var start=((range.split("="))[1]).split("-")[0];
 		start = parseInt(start)
-		
-		console.log(start);
-		console.log(size);
 		var end = ((range.split("="))[1]).split("-")[1]
-		console.log(end)
 		if(!end)end=Math.min(size-1,start+1000)
 		end=parseInt(end)
-		console.log(end);	
+		
 
 		headers ={
 			'Content-Range':'bytes '+ start+'-'+end+'/'+size,
@@ -36,10 +29,9 @@ app.get("/video",function(request,response){
 			'Content-Length': end-start+1+"",
         	'Content-Type': 'video/mp4'
 		}
-		//console.log(headers);
+		
 		response.writeHead(206,headers);
 		const file = fs.createReadStream(filePath, {start,end});
-		console.log(file)
 		file.pipe(response)
 	}
 
